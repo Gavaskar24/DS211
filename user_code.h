f3c95912ed9175bc060a4afbf0892989eb5343ba
Gavaskar <mgavaskar24@gmail.com>
2023-09-10 12:22:57 +0530

    created a new test.cpp to work out problem 1

--- user_code.h
+++ user_code.h
@@ -4,22 +4,19 @@
 //         columnIndices - a 1D array of integers
 //          rowPointers - a 1D array of integers
 //         (see the assignment description for details)
-
-denseToSparse(denseMatrix, values, columnIndices, rowPointers){
-    int n = denseMatrix.size();
-    int m = denseMatrix[0].size();
-    int count = 0;
-    for(int i = 0; i < n; i++){
-        rowPointers.push_back(count);
-        for(int j = 0; j < m; j++){
-            if(denseMatrix[i][j] != 0){
-                values.push_back(denseMatrix[i][j]);
-                columnIndices.push_back(j);
-                count++;
-            }
-        }
-    }
-    rowPointers.push_back(count);
-    
-    
+void denseToSparse(const std::vector<std::vector<int>>& denseMatrix, std::vector<int>& values, std::vector<int>& columnIndices, std::vector<int>& rowPointers) {
+   int n = denseMatrix.size();
+   int m = denseMatrix[0].size();
+   int count = 0;
+   for(int i = 0; i < n; i++) {
+       rowPointers.push_back(count);
+       for(int j = 0; j < m; j++) {
+           if(denseMatrix[i][j] != 0) {
+               values.push_back(denseMatrix[i][j]);
+               columnIndices.push_back(j);
+               count++;
+           }
+       }
+   }
+   rowPointers.push_back(count);
 }
